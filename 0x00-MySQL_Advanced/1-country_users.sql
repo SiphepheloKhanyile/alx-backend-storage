5b4889efce95b7254414776a907447770f4d694b
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-06-19 22:06:12 +0200

    add: Task 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- creates a table `users`
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE IF NOT EXISTS users(
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US'
+);
