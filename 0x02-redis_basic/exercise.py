2833ff4fc2329283c448d96ab5dedb21002ba20d
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-06-28 20:24:21 +0200

    add: Combined tasks

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,6 +7,42 @@ from typing import Union, Optional, Callable
 import redis
 
 
+def count_calls(method: Callable) -> Callable:
+    """
+    Decorator to count the number of times a method is called.
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        Wrapper function for counting calls.
+        """
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """
+    Decorator to store the history of inputs and outputs for a function.
+    """
+    key = method.__qualname__
+    inputs = key + ":inputs"
+    outputs = key + ":outputs"
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        Wrapper function for storing history.
+        """
+        self._redis.rpush(inputs, str(args))
+        data = method(self, *args, **kwargs)
+        self._redis.rpush(outputs, str(data))
+        return data
+
+    return wrapper
+
+
 class Cache:
     """
     Creates instance of Redis Client
@@ -16,6 +52,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Args:
@@ -71,3 +109,28 @@ class Cache:
             return int(value.decode('utf-8')) if value else 0
         except ValueError:
             return 0
+
+
+def replay(method: Callable) -> None:
+    """
+    Replay the history of a function.
+
+    Args:
+        method (Callable): The function to be replayed.
+
+    Returns:
+        None
+    """
+    name = method.__qualname__
+    cache = redis.Redis()
+    calls = cache.get(name)
+    if calls:
+        calls = calls.decode("utf-8")
+        print("{} was called {} times:".format(name, calls))
+        inputs = cache.lrange(name + ":inputs", 0, -1)
+        outputs = cache.lrange(name + ":outputs", 0, -1)
+        for i, o in zip(inputs, outputs):
+            print("{}(*{}) -> {}".format(name, i.decode('utf-8'),
+                                         o.decode('utf-8')))
+    else:
+        print("{} has not been called yet.".format(name))
